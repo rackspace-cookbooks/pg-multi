e140d2a92367a4ff40da7fed38b1f6a17ef081df
Christopher Coffey <christopher.coffey@rackspace.com>
2014-07-07 14:35:13 -0500

    update README

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 pg-multi
 ========
 
-Postgresql Master/Slave Chef wrapper cookbook
+Postgresql Master/Slave Chef Streaming Replication wrapper cookbook - creates a master server and a read-only slave system.
+
+
---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name 'pg-multi'
+maintainer 'Christopher Coffey'
+maintainer_email 'nomad@cybermerc.org'
+license 'Apache 2.0'
+description 'Postgresql Master/Slave Streaming Replication wrapper cookbook'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version '0.1.0'
+
