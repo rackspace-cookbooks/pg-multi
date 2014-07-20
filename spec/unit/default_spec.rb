5f61a5be1ebcb6ca094ae6daae5c528de415b640
Christopher Coffey <christopher.coffey@rackspace.com>
2014-07-19 22:31:41 -0500

    corrected unit test

--- spec/unit/default_spec.rb
+++ spec/unit/default_spec.rb
@@ -1,28 +1,27 @@
 # encoding: UTF-8
-
 require 'spec_helper'
 
-platforms = {
-  'ubuntu' => ['12.04', '14.04'],
-  'centos' => ['6.5']	
-}
-
 describe 'pg-multi::default' do
-  let(:chef_run) do
-    ChefSpec::Runner.new do |node|
-      node.set['postgresql']['password'] = 'passwurd'
-      node.set['postgresql']['version'] = ['9.3']
-    end.converge(described_recipe)
+  before do
+    allow(::File).to receive(:symlink?).and_return(false)
   end
+
+  platforms = {
+    'ubuntu' => ['12.04', '14.04'],
+    'centos' => ['6.5'] 
+  }
 
   platforms.each do |platform, versions|
     versions.each do |version|
       context "on #{platform.capitalize} #{version}" do
         let(:chef_run) do
-          ChefSpec::Runner.new(:platform => platform, :version => version).converge(described_recipe)
+          ChefSpec::Runner.new(:platform => platform, :version => version) do |node|
+            node.set['postgresql']['password']['postgres']= 'test123'
+            node.set['postgresql']['version'] = '9.3'
+          end.converge(described_recipe)
         end
 
-        it 'includes the `postgresql::server` recipe' do
+        it 'includes postgresql::server recipe' do
           expect(chef_run).to include_recipe('postgresql::server')
         end
       end
